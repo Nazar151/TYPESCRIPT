f1d4f5e7bc0dbd65623f949908136d601c64469f
Nazar Volkov <nazar.volkov@gmail.com>
2021-05-27 23:55:56 +0200

    array + any + function + union + value_set + type_check + void + unknown +

--- app.js
+++ app.js
@@ -1,11 +1,95 @@
-var isMale = true;
-var isFemale = false;
-var age = 35;
-var weight = 83;
-var imaginarySalary = 123447865;
-var firstName = "Nazar";
-var lastName = 'Volkov';
-var someText = "This my name: " + firstName + ", and lastname " + lastName;
-console.log(someText);
 var arr = [];
 arr.push(34);
+console.log(arr);
+var touple = ["Nazar", 35];
+touple.push(34);
+touple.push("dfg");
+// touple.push(true)  - boolean додати не можемо
+//можемо додавати тількі ті типи котрі описанні в таблиці
+//-------------------------ANY------------------------------
+// використовуємо для міграції з старого коду JS на TS
+var xxx;
+xxx = "Nazar";
+xxx = 3;
+xxx = {};
+xxx = function () { };
+var xero = xxx;
+xxx.age;
+xxx();
+//----------FUNCTION---------------
+function sum(a, b) {
+    return (a + b);
+}
+var bar = sum(10, 10);
+console.log(bar);
+var suma = function (a, b) { return a + b; };
+//------------UNION TYPE-----------------
+var ux;
+ux = 5;
+ux = "h";
+var uz;
+var f = function (x) {
+    return "";
+};
+//--------------VALUE_SET--------------
+var flagIsSet;
+var myName;
+var age;
+var someVar;
+var anotherFlag;
+var dice;
+dice = 5;
+// dice = 8;   --- не можемо приписати до змінної 8, бо її не має в закресі чисел які можемо приписати
+var yName;
+yName = "Nazar";
+// yName = "Viktor";
+console.log(yName);
+//---------------TYPE_CHECK----------
+{
+    var logical = true;
+    var age_1 = 3;
+    var name_1 = "nazar";
+    if (typeof age_1 === "number") {
+        console.log("age is number");
+    }
+    var something = void 0;
+    if (typeof something === "string") {
+        something.length;
+    }
+    else if (typeof something === "number") {
+    }
+    else if (typeof something === "function") {
+        something();
+    }
+}
+// --------------VOID--------------------
+function printText(text) {
+    console.log(text);
+}
+// let person : void = undefined;  ---- брак вартсті
+//-----------------UNKNOWN----------------
+{
+    var xxx_1;
+    xxx_1 = "Nazar";
+    xxx_1 = 3;
+    xxx_1 = {};
+    xxx_1 = function () { };
+    var xero_1 = xxx_1;
+    xxx_1.age;
+    xxx_1();
+    if (typeof xxx_1 === "number") {
+        var xage = xxx_1;
+    }
+    ;
+    if (typeof xxx_1 === "object" && xxx_1 !== null) {
+        if (xxx_1.age !== undefined) {
+        }
+        ;
+    }
+    ;
+    if (typeof xxx_1 === "function") {
+        xxx_1();
+    }
+    ;
+}
+;
--- app.ts
+++ app.ts
@@ -2,4 +2,125 @@ let arr : number [] = [];
 
 arr.push(34);
 
-const touple : [string, number] = ["Nazar", 35]
+console.log(arr);
+
+const touple : [string, number] = ["Nazar", 35];
+
+touple.push(34);
+touple.push("dfg");
+// touple.push(true)  - boolean додати не можемо
+//можемо додавати тількі ті типи котрі описанні в таблиці
+
+//-------------------------ANY------------------------------
+// використовуємо для міграції з старого коду JS на TS
+let xxx : any;
+xxx = "Nazar";
+xxx = 3;
+xxx = {};
+xxx = () => {};
+
+let xero : number = xxx;
+xxx.age;
+xxx();
+
+//----------FUNCTION---------------
+
+function sum(a : number, b : number) : number {
+    return (a + b);
+}
+let bar = sum(10, 10);
+console.log(bar);
+
+const suma = (a : number, b : number) => a + b;
+
+//------------UNION TYPE-----------------
+
+let ux : string | number;
+
+ux = 5;
+ux = "h"
+
+let uz : string | number | boolean;
+
+const f = (x : string | boolean) : number | string => {
+    return "";
+}
+
+//--------------VALUE_SET--------------
+
+let flagIsSet : boolean;
+let myName : string;
+let age : number;
+
+let someVar : string | number;
+
+let anotherFlag : true | false;
+
+let dice : 1 | 2 | 3 | 4 | 5 | 6 ;
+dice = 5;
+// dice = 8;   --- не можемо приписати до змінної 8, бо її не має в закресі чисел які можемо приписати
+let yName : "Nazar" | "Illia" | "Alex";
+
+yName = "Nazar";
+// yName = "Viktor";
+console.log(yName);
+
+
+//---------------TYPE_CHECK----------
+{
+    let logical: boolean = true;
+    let age: number = 3;
+    let name: string = "nazar";
+
+    if (typeof age === "number") {
+        console.log("age is number")
+    }
+
+    let something: any;
+    if (typeof something === "string") {
+        something.length;
+    } else if (typeof something === "number") {
+    } else if (typeof something === "function") {
+        something();
+
+    }
+    }
+
+    // --------------VOID--------------------
+
+
+function printText(text : string) : void {
+    console.log(text);
+}
+
+// let person : void = undefined;  ---- брак вартсті
+
+//-----------------UNKNOWN----------------
+{
+    let xxx: any;
+    xxx = "Nazar";
+    xxx = 3;
+    xxx = {};
+    xxx = () => {};
+
+    let xero: number = xxx;
+    xxx.age;
+    xxx();
+
+    if(typeof xxx === "number") {
+        let xero : number = xxx;
+    };
+
+    if(typeof xxx === "object" && xxx !== null) {
+        if ((xxx as { age: number }).age !== undefined) {
+        };
+    };
+
+   if(typeof  xxx === "function"){
+       xxx();
+   };
+};
+
+
+
+
