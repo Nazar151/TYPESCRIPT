9b657a1a1695fa3af148fdd27d02677dd4339dde
Nazar Volkov <nazar.volkov@gmail.com>
2021-05-27 22:03:04 +0200

    type

---
+++ app.js
@@ -0,0 +1,9 @@
+var isMale = true;
+var isFemale = false;
+var age = 35;
+var weight = 83;
+var imaginarySalary = 123447865;
+var firstName = "Nazar";
+var lastName = 'Volkov';
+var someText = "This my name: " + firstName + ", and lastname " + lastName;
+console.log(someText);
---
+++ app.ts
@@ -0,0 +1,13 @@
+let isMale : boolean = true;
+let isFemale : boolean = false;
+
+let age :  number = 35;
+let weight : number = 83;
+let imaginarySalary : number = 1_2344_7865;
+
+let firstName : string = "Nazar";
+let lastName : string = 'Volkov';
+
+let  someText : string = `This my name: ${firstName}, and lastname ${lastName}`;
+console.log(someText)
+
